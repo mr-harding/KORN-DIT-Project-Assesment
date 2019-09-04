a04dca5f9e9132e1bf85de6433ead08ffc2a86cc
KornSuwannagit <16ksuwannagit@wakatipu.school.nz>
2019-09-05 09:52:37 +1200

    Fixing script

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ $("#slideshow > div:gt(0)").hide();
 
 setInterval(function() {
   $('#slideshow > div:first')
-    .fadeOut(2000)
+    .fadeOut(1000)
     .next()
-    .fadeIn(2000)
+    .fadeIn(1000)
     .end()
     .appendTo('#slideshow');
 }, 5000);
